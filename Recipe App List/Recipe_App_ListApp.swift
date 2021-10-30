a5aa4cd8f915d57fddd0998baf993d6f434cce04
Manachem Martinov <70970712+MenachemMartinov@users.noreply.github.com>
2021-10-30 20:16:38 +0200

    Initial Commit

---
+++ Recipe App List/Recipe_App_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_App_ListApp.swift
+//  Recipe App List
+//
+//  Created by מנחם מרטינוב on 30/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_App_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
